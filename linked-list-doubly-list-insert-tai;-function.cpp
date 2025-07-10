8896a36922d4a085e48be030410d2a160cf7a42a
riya-sharma-sketch <riyagssharma03@gmail.com>
2025-07-10 11:10:11 +0530

    Create linked-list-doubly-list-insert-tai;-function.cpp

---
+++ linked-list-doubly-list-insert-tai;-function.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class node{
+	
+	public:
+		int data;
+		node* prev;
+		node* next;
+		
+		node(int d): data(d), prev(NULL), next(NULL){}
+};
+
+void fprint(node* head){
+	
+	node* temp = head;
+	
+	while( temp != NULL ){
+		
+		cout<<head->prev<<"\t" <<head->data <<"\t"<< head->next<<endl;
+		temp = temp->next;
+	}
+}
+
+void insertTail( node* &head, int val){
+	
+	node* newnode = new node(val);
+	
+	if( head == NULL ){
+		
+		head = newnode;
+		return;
+	}
+	
+	node* temp = head;
+	while( temp->next != NULL )
+		temp = temp->next;
+	
+	newnode->prev = temp;	
+	temp->next = newnode;
+}
+
+int main(){
+	
+	node* head = new node(10);
+	
+	insertTail( head, 20 );
+	insertTail( head, 30 );
+	insertTail( head, 40 );
+	insertTail( head, 50 );
+	
+	fprint( head );
+	cout<<endl;
+	
+	return 0;
+}
