422f55d063f9179bf0442a3bb625c7eadb17a198
riya-sharma-sketch <riyagssharma03@gmail.com>
2025-07-10 13:54:13 +0530

    Create pair.cpp

---
+++ pair.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	
+//	pair < data_type, data_type> pair_name;
+
+	pair <char, int> p;
+	
+	p.first = 'A';
+	p.second = 1;
+	
+	cout<< p.first <<" ";
+	cout<< p.second <<endl;
+	
+	
+	return 0;
+}
