c9b860d4dea2d29da253468e46baea8b8508afaa
riya-sharma-sketch <riyagssharma03@gmail.com>
2025-07-11 10:53:44 +0530

    Create pointer8.cpp

---
+++ pointer8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void func( int& num ){
+	
+	num = 10;
+}
+
+int main(){
+	
+	int num = 20;
+	
+	cout<<num <<endl;	// 20
+	func( num );	// call by value
+	cout<< num <<endl;	// 20
+		
+	
+	return 0;
+}
